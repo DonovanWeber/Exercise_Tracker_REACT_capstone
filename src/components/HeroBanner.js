6cbfed5da523b7355bef673a129f93f63bba6eb9
Donovan Weber <donovanweber03@gmail.com>
2022-09-23 12:15:39 -0400

    WIP HeroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+      mt: { lg: '212px', xs:"70px" }
+    }}>
+      <Typography
+        color="#FF2625"
+        fontWeight="600"
+        fontSize="26px">
+          WorkoutWeaver
+
+      </Typography>
+    </Box>
   )
 }
 
